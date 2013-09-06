62a3d0b791e940f94eec45bae791df4cb7667753
Srdjan Prodanovič <s1e@cuf.si>
2013-09-06 20:10:50 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Popupdom::Application.config.secret_key_base = 'a35ae929ad783d4b7433ce7e05feb1f705246661fe17412eee1c202c03203db1f349393906112e77ed50d3181be3ae2a8177c0e4734225dc96d3c81ad287ccb9'
