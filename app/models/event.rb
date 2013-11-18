a4abfd5167123048511dc7c7a9147998ea2dc3b1
Srdjan Prodanovič <s1e@cuf.si>
2013-11-18 22:44:17 +0100

    Fix event ordering with default_scope. Enter correct popupdom date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :event_category
 
-  default_scope :order => :begins_at
+  default_scope order('begins_at ASC')
 
   has_many :event_images
   accepts_nested_attributes_for :event_images, reject_if: lambda {|attributes| attributes['file'].blank?}, allow_destroy: true
