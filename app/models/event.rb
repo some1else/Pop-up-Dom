fd9a3a76ed9e77dcabfa3d0085d80faf97a3b44b
Srdjan Prodanovič <s1e@cuf.si>
2013-11-18 12:49:13 +0100

    Order events by date (default scope)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to :event_category
 
+  default_scope :order => :begins_at
+
   has_many :event_images
   accepts_nested_attributes_for :event_images, reject_if: lambda {|attributes| attributes['file'].blank?}, allow_destroy: true
 end
